1247fd58db8d2020e8a9048d5c27202ee61c6f4e
egoomoy <egoomoy@gmail.com>
2022-08-07 18:00:40 +0900

    etc

--- src/main/java/aehdb/comm/security/WebConfig.java
+++ src/main/java/aehdb/comm/security/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5501", "http://localhost:8081");
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:8081");
     }
 }
--- src/main/java/aehdb/comm/security/WebSecurityConfig.java
+++ src/main/java/aehdb/comm/security/WebSecurityConfig.java
@@ -27,7 +27,6 @@ public class WebSecurityConfig {
 	static class ChatSecurityConfig extends WebSecurityConfigurerAdapter {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			System.out.println("qwrqwr");
 			http.csrf()
 					.disable()
 					.formLogin()
@@ -50,9 +49,6 @@ public class WebSecurityConfig {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			
-			System.out.println("200");
-
 			http.csrf()
 					.disable() // jwt토큰을 사용하여 쿠키에 의존하면, csrf에서 관련이 몹시 적다. 브라우저 검사를 해보면 알겠지만, 요청하는 페이지의 쿠키만 보여
 					.formLogin()
